7b1bc48984361648710d1f7ebb00a87788a5ea81
Benjie Gatt <benjie332@gmail.com>
2014-04-19 19:02:04 +0200

    Added first design of main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('kelmaApp')
-  .controller('MainCtrl', ['$scope', 'Kelma', function ($scope, Kelma) {
-    $scope.kelma = Kelma.latest();
-  }]);
+    .controller('MainCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
+        /*$scope.kliem = Restangular.all('kliem').getList().$object;
+        var param = 'latest';
+        $scope.kelma = Restangular.one('kliem', 'query').getList({'test': 'param'}).$object;*/
+        $scope.kelma = {kelma: 'KELMA', ezempju: 'Bhal meta nghidu bla bla bla', data: '2014-01-01'};
+}]);
