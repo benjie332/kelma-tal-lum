869b4764e0b487403ba492207b12656bbad37b6b
Benjie Gatt <benjie332@gmail.com>
2014-04-27 13:48:16 +0200

    Fixed date issues when querying server

--- app/scripts/controllers/kelma-tal-lum.js
+++ app/scripts/controllers/kelma-tal-lum.js
@@ -2,7 +2,7 @@
 
 angular.module('kelmaTalLumApp')
     .controller('KelmaController', ['$scope', '$moment', 'KelmaTalLum', function ($scope, $moment, KelmaTalLum) {
-        KelmaTalLum.query({data: $moment()}, function(kelma) {
+        KelmaTalLum.query({data: $moment().toISOString()}, function(kelma) {
             if (kelma) {
                 $scope.kelma = {kelma : kelma._id, ezempju: kelma.ezempju, data: $moment(kelma.data).format('YYYY-MM-DD')};
             } else {
