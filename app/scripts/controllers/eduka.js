5c638c86fd6240e63f5cd46e01b2486760b85db5
Benjie Gatt <benjie332@gmail.com>
2014-05-11 23:15:33 +0200

    Il-Kumitat qed jirfina l-metodi ta approvazzjoni tieghu

--- app/scripts/controllers/eduka.js
+++ app/scripts/controllers/eduka.js
@@ -5,22 +5,43 @@ angular.module('kelmaTalLumApp')
 
         $scope.kliem = [];
         $scope.ilKandidat = {};
+        $scope.kelmaTalLum = null;
 
-        Kelma.talLum({data: $moment().toISOString()}, function(kelma) {
-            $scope.kelmaTalLum = kelma;
+        Kelma.talLum({data: $moment().toISOString()}, function (kelma) {
+            console.log('kelma', kelma);
+            //TODO: have a factory or something do this somewhere centralised - its duplicated in the main page
+            if (kelma._id) {
+                $scope.kelmaTalLum = kelma;
+            } else {
+                $scope.kelmaTalLum = null;
+            }
         });
 
-        Kelma.suggerimenti({}, function(suggerimenti) {
+        Kelma.suggerimenti({}, function (suggerimenti) {
             $scope.suggerimenti = suggerimenti;
         });
 
-        $scope.nikkontemplaw = function(suggeriment) {
+        $scope.nikkontemplaw = function (suggeriment) {
             $scope.ilKandidat = suggeriment;
         };
 
-        $scope.eduka = function() {
+        $scope.eduka = function () {
             $scope.ilKandidat.data = Date.now();
-            $scope.ilKandidat.$save();
-            NotificationService.success('Il-Poplu ghad jirringrazzjak tal-ghazla tieghek');
+            $scope.ilKandidat.$save(function () {
+                    Kelma.suggerimenti({}, function (suggerimenti) {
+                        $scope.suggerimenti = suggerimenti;
+                    });
+                    $scope.kelmaTalLum = $scope.ilKandidat;
+                    $scope.ilKandidat = {};
+                    NotificationService.success('Il-Poplu ghad jirringrazzjak tal-ghazla tieghek');
+                }, function (err) {
+                    NotificationService.error(err);
+                }
+            );
+
         };
-}]);
+
+        $scope.irranga = function (suggeriment) {
+            suggeriment.$save();
+        }
+    }]);
--- app/scripts/controllers/kelma-tal-lum.js
+++ app/scripts/controllers/kelma-tal-lum.js
@@ -3,7 +3,7 @@
 angular.module('kelmaTalLumApp')
     .controller('KelmaController', ['$scope', '$moment', 'Kelma', function ($scope, $moment, Kelma) {
         Kelma.talLum({data: $moment().toISOString()}, function(kelma) {
-            if (kelma) {
+            if (kelma._id) {
                 $scope.kelma = {id: kelma._id, kelma : kelma.kelma, ezempju: kelma.ezempju, data: $moment(kelma.data).format('YYYY-MM-DD')};
             } else {
                 $scope.kelma = {};
